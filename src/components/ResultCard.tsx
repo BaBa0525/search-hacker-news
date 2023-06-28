9f7d5a4fd9c75fde1113ae3056c9262f3c2eaa02
BaBa0525 <hsunhsun0525@gmail.com>
2023-06-28 20:09:15 +0800

    ui: add icons to provide more information

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -1,5 +1,10 @@
 import type { Hit } from "@/types/api";
 import { sanitize } from "dompurify";
+import { AiOutlineHeart } from "react-icons/ai";
+import { BsClock } from "react-icons/bs";
+import { GoComment } from "react-icons/go";
+import { MdArticle } from "react-icons/md";
+import { PiUserLight } from "react-icons/pi";
 
 type ResultCardProps = {
   hit: Hit;
@@ -15,20 +20,65 @@ export const ResultCard: React.FC<ResultCardProps> = ({ hit }) => {
       })
     : hit.title;
 
+  const author = hit._highlightResult.author
+    ? sanitize(hit._highlightResult.author.value, {
+        USE_PROFILES: { html: true },
+      })
+    : hit.author;
+
   return (
     <a
-      className="flex flex-col gap-2 p-2 bg-white rounded group hover:bg-search-line hover:text-white"
+      className="flex items-center gap-4 p-2 bg-white rounded group hover:bg-search-line hover:text-white"
       href={hit.url}
     >
-      <h4
-        className="text-lg font-medium SearchHighlight"
-        dangerouslySetInnerHTML={{ __html: title }}
-      />
-      <p className="text-sm font-light text-gray-600 group-hover:text-white">{`${
-        hit.points
-      } points by ${hit.author} on ${formatter.format(createdAt)} | ${
-        hit.num_comments
-      } comments`}</p>
+      <MdArticle className="flex-shrink-0 w-6 h-6 ml-2 text-search-line group-hover:text-white" />
+      <article className="flex flex-col flex-1 min-w-0">
+        <h4
+          className="font-medium truncate SearchHighlight"
+          dangerouslySetInnerHTML={{ __html: title }}
+        />
+        <div className="flex gap-4 text-sm font-light text-gray-600 group-hover:text-white">
+          <div className="flex gap-1">
+            <AiOutlineHeart className="h-full" />
+            <span>{hit.points}</span>
+          </div>
+          <div className="flex gap-1">
+            <PiUserLight className="h-full" />
+            <span
+              className="SearchHighlight"
+              dangerouslySetInnerHTML={{ __html: author }}
+            />
+          </div>
+          <div className="flex gap-1">
+            <BsClock className="h-full" />
+            <span>{formatter.format(createdAt)}</span>
+          </div>
+          <div className="flex gap-1">
+            <GoComment className="h-full" />
+            <span>{hit.num_comments}</span>
+          </div>
+        </div>
+      </article>
     </a>
   );
 };
+
+/**
+ * <a
+      className="flex items-center gap-4 p-2 bg-white rounded group hover:bg-search-line hover:text-white"
+      href={hit.url}
+    >
+      <MdArticle className="flex-shrink-0 w-6 h-6 ml-2 text-search-line group-hover:text-white" />
+      <article className="flex flex-col flex-1 min-w-0">
+        <h4
+          className="font-medium truncate SearchHighlight"
+          dangerouslySetInnerHTML={{ __html: title }}
+        />
+        <p className="text-sm font-light text-gray-600 group-hover:text-white">{`${
+          hit.points
+        } points by ${author} on ${formatter.format(createdAt)} | ${
+          hit.num_comments
+        } comments`}</p>
+      </article>
+    </a>
+ */
--- src/components/SearchTable.tsx
+++ src/components/SearchTable.tsx
@@ -2,6 +2,8 @@ import { getSearchResult } from "@/services";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import { MdClose } from "react-icons/md";
+import { NotFoundIcon } from "./NotFoundIcon";
 import { ResultCard } from "./ResultCard";
 
 export const SearchTable: React.FC = () => {
@@ -10,13 +12,19 @@ export const SearchTable: React.FC = () => {
   return (
     <div className="flex flex-col max-w-full max-h-full py-4 rounded-md drop-shadow bg-search-bg">
       <div className="flex items-center flex-shrink-0 h-12 gap-2 px-4 m-4 bg-white border-2 rounded-md border-search-line">
-        <FiSearch className="w-6 h-6 text-search-line" />
+        <FiSearch className="flex-shrink-0 w-6 h-6 text-search-line" />
         <input
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           className="w-full h-full text-lg outline-none"
           placeholder="Search news"
         />
+        {query !== "" && (
+          <MdClose
+            className="w-6 h-6 cursor-pointer hover:text-search-line"
+            onClick={() => setQuery("")}
+          />
+        )}
       </div>
       <SearchResults query={query} />
     </div>
@@ -55,17 +63,20 @@ const SearchResults: React.FC<SearchResultsProps> = ({ query }) => {
     return <div>Error</div>;
   }
 
-  if (data?.pages.length === 0) {
+  if (data?.pages[0].nbHits === 0) {
     return (
-      <div>
-        No results were found. Check your spelling or try different keywords.
-      </div>
+      <section className="flex flex-col items-center gap-2 py-10 mx-6 overflow-auto text-sm select-none text-search-help">
+        <NotFoundIcon />
+        <h3>
+          No results were found. Check your spelling or try different keywords.
+        </h3>
+      </section>
     );
   }
 
   return (
     <section className="px-6 overflow-y-auto">
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-2">
         {data?.pages.map((page) => (
           <div key={page.page} className="flex flex-col gap-2">
             {page.hits.map((hit) => (
