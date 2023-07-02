7c9c4eaab2ce9b2f53243c740de6ec2c32809d34
BaBa0525 <hsunhsun0525@gmail.com>
2023-07-02 15:44:26 +0800

    refactor: add reusability to infinite scroll

--- src/components/InfiniteScroll.tsx
+++ src/components/InfiniteScroll.tsx
@@ -1,72 +1,50 @@
+import { useFocusResult } from "@/hooks/useFocusResult";
 import { useLastElementRef } from "@/hooks/useLastElementRef";
-import type { SearchResults } from "@/types/api";
-import type {
-  FetchNextPageOptions,
-  InfiniteQueryObserverResult,
-} from "@tanstack/react-query";
-import { ResultCard } from "./ResultCard";
+import type { ComponentType } from "react";
 
-type InfiniteScrollProps = {
-  pages: SearchResults[];
-  next: (
-    options?: FetchNextPageOptions | undefined
-  ) => Promise<InfiniteQueryObserverResult<SearchResults, unknown>>;
-  hasMore: boolean;
+export type InfiniteScrollProps<T> = {
+  pages: T[];
+  getPageResult: (index: number, array: T[]) => unknown[];
+  hasNextPage: boolean;
+  fetchNextPage: () => void | Promise<void>;
+  children: ComponentType<{
+    page: T;
+    isLastPage: boolean;
+    lastElementRef: (node: HTMLElement | null) => void;
+    focusedResult?: number;
+  }>;
 };
 
-export const InfiniteScroll: React.FC<InfiniteScrollProps> = ({
+export const InfiniteScroll = <T,>({
   pages,
-  next,
-  hasMore,
-}) => {
-  const lastElementRef = useLastElementRef({ hasMore, next });
+  hasNextPage,
+  getPageResult,
+  fetchNextPage,
+  children: Component,
+}: InfiniteScrollProps<T>) => {
+  // InfiniteScroll observer
+  const lastElementRef = useLastElementRef({
+    hasMore: hasNextPage,
+    next: fetchNextPage,
+  });
+
+  // Add event listener to arrow keys
+  const { focusedPage, focusedResult } = useFocusResult({
+    pages,
+    getPageResult,
+  });
 
   return (
     <div className="flex flex-col gap-4">
       {pages.map((page, pageIndex) => (
-        <PageSection
-          key={pageIndex}
-          page={page}
+        <Component
           isLastPage={pageIndex === pages.length - 1}
           lastElementRef={lastElementRef}
+          page={page}
+          focusedResult={focusedPage === pageIndex ? focusedResult : undefined}
+          key={pageIndex}
         />
       ))}
-    </div>
-  );
-};
-
-type PageSectionProps = {
-  page: SearchResults;
-  isLastPage: boolean;
-  lastElementRef: (node: HTMLLIElement | null) => void;
-};
-
-const PageSection: React.FC<PageSectionProps> = ({
-  page,
-  isLastPage,
-  lastElementRef,
-}) => {
-  const resultStart = page.hitsPerPage * page.page + 1;
-  const resultEnd = page.hitsPerPage * page.page + page.hits.length;
-
-  return (
-    <div key={page.page} className="flex flex-col">
-      <h4 className="text-xs font-semibold md:text-sm text-search-line-light dark:text-search-line-dark">
-        {`Result ${resultStart} ~ ${resultEnd}`}
-      </h4>
-      <ul className="flex flex-col gap-2">
-        {page.hits.map((hit, postIndex) =>
-          isLastPage && page.hits.length === postIndex + 2 ? (
-            <li ref={lastElementRef} key={hit.objectID}>
-              <ResultCard key={hit.objectID} hit={hit} />
-            </li>
-          ) : (
-            <li key={hit.objectID}>
-              <ResultCard key={hit.objectID} hit={hit} />
-            </li>
-          )
-        )}
-      </ul>
     </div>
   );
 };
